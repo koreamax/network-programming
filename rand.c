390c646ee4ecc4ad1b66650c8f012efee3a1a797
koreamax <koreamax012@gmail.com>
2026-03-19 19:42:12 +0900

    program simulate

--- rand.c
+++ rand.c
@@ -146,10 +146,10 @@ int main()
         printf("%f ", exponential(0.1));
     }
     */
-
+    /*
     // 고객이 랜덤하게 도착하고 은행원이 한 명씩 처리하는 시스템
 
-    int Te = 10000000; // 은행 전체 시간
+    int Te = 200; // 은행 전체 시간
     int t1, t2, time; // 다음 고객 도착 시간, 현재 서비스 종료 시간, 현재 시간
     int Ta, Ts;
     double Pa = 0.1, Ps = 0.1; // 도착 확률, 서비스 확률
@@ -159,7 +159,9 @@ int main()
     double mean_time = 0; // mean number의 적분
     int next_time; // 다음 시간이 t1인지 t2인지 예측
 
+    
     n = 0; t1 = 0; t2 = Te; time = 0;
+    
     while (time < Te) { // 은행이 문 닫을 때까지 
         next_time = (t1 < t2) ? t1 : t2; // t1, t2 중 하나
         mean_time += (double)n * (next_time - time); // 적분
@@ -190,8 +192,78 @@ int main()
             }
         }
     }
+    */
+    /* 
+    // switch-case문으로 바꿔서 만들기
+    int event;
+    while (time < Te) {
+        if (t1 < t2) {
+            next_time = t1;
+            event = 1;
+        } else {
+            next_time = t2;
+            event = 3;
+        }
+        mean_time += (double)n * (next_time - time);
+        time = next_time;
+        switch (event) {
+            case 1: // arrive
+                n++;
+                cnt_arri++;
+                printf("at time %d, customer %d arrive\n", time, cnt_arri);
+                t1 = time + geometric(Pa);
+                if (n == 1) {
+                    event = 2;
+                    goto BEGIN;
+                }
+                break;
+            case 2: // begin
+        BEGIN:
+                cnt_off++;
+                printf("at time %d, customer %d begin\n", time, cnt_off);
+                t2 = time + geometric(Ps);
+                res_time += t2 - time;
+                break;
+            case 3: // end
+                printf("at time %d, customer %d end\n", time, cnt_off);
+                n--;
+                if (n > 0) {
+                    event = 2;
+                    goto BEGIN;
+                } else {
+                    t2 = Te;
+                }
+                break;
+    }
+    */
+    /* 
+    // 통계 구하기
     printf("throughput = %f\n", (double)cnt_arri / Te);
     printf("utilization = %f\n", (double)res_time / Te);
     printf("mean no. in system = %f\n", mean_time / Te);
     printf("mean residence time = %f", mean_time / cnt_arri);
+    */
+
+    // 프로그램 실행 흐름을 확률적으로 따라가가
+    double sum = 0;
+    int tries = 1000000;
+    for (int i = 0; i < tries; i++) {
+        int n = 1;
+        int e = 2;
+        while(e < 9) {
+            switch(e) {
+                case 1: e = 2; n++; break;
+                case 2: e = (uniform(0, 1) < 0.2) ? 3 : 5; n++; break;
+                case 3: e = 4; n++; break;
+                case 4: e = (uniform(0, 1) < 0.25) ? 3 : 7; n++; break;
+                case 5: e = (uniform(0, 1) < 0.45) ? 6 : 7; n++; break;
+                case 6: e = 7; n++; break;
+                case 7: e = 8; n += 2; break;
+                case 8: e = (uniform(0, 1) < 0.3) ? 1 : 9; break;
+            }
+        }
+        sum += n;
+    }
+    printf("%f", sum / tries);
+    
 }
